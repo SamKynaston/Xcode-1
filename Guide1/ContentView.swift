af4227fd6a2b0f2cfda86c18c54d2dffd1e5e6e2
samkynaston <samuelkynno@gmail.com>
2025-03-11 11:55:28 +0000

    made font larger

--- Guide1/ContentView.swift
+++ Guide1/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text(text)
+                .font(.largeTitle)
                 .padding()
             
             if (isButtonVisible) {
