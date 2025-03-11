cf26ac918c27fc21c9cdc70c7c5ef36b9dbce8b7
samkynaston <samuelkynno@gmail.com>
2025-03-11 11:25:55 +0000

    Initial Commit

---
+++ Guide1/Guide1App.swift
@@ -0,0 +1,17 @@
+//
+//  Guide1App.swift
+//  Guide1
+//
+//  Created by Sam Kynaston on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Guide1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Guide1Tests/Guide1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Guide1Tests.swift
+//  Guide1Tests
+//
+//  Created by Sam Kynaston on 11/03/2025.
+//
+
+import Testing
+
+struct Guide1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
